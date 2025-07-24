b8130990ec2b928b3ba0fa7d5c5c4f69b179f6ed
MozzerGrozzer <grozzermozzer@gmail.com>
2025-07-24 18:51:58 -0300

    feat: working timer check for JS :holdingtears:(#10)

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,57 @@
-let clientNumberMorning = document.getElementById("clientNumberMorning")
-let clientNumberEvening=document.getElementById('clientNumberEvening')
-let clientNumberNight=document.getElementById('clientNumberNight')
-let manha=document.getElementsByClassName('manha')
-let evening=document.getElementsByClassName('evening')
-let night=document.getElementsByClassName('night')
+// let clientNumberMorning = document.getElementById("clientNumberMorning")
+// let clientNumberEvening=document.getElementById('clientNumberEvening')
+// let clientNumberNight=document.getElementById('clientNumberNight')
+// let manha=document.getElementsByClassName('manha')
+// let evening=document.getElementsByClassName('evening')
+// let night=document.getElementsByClassName('night')
 
-const clients=[{
+
+/* timeCheck(hour) is to automaticaly put each user on HTML
+ calendar by the time the user put it there
+so: morning, evening and night 
+theres a closed one so user dont choose a hour where the barber isnt
+working
+*/
+
+const timeCheck=(hour)=>{
+    if(hour>=8 && hour <12){return 'morning'}
+        else if (hour>=12 && hour<18){return 'evening'}
+            else if (hour >= 18 && hour<=21){return 'night'}
+    else{'closed'}
+}
+
+// this adds a "half hour"
+let slots = []
+for (let h= 8; h<=21; h++){
+    slots.push({hour: h, minute:0,period:timeCheck(h)});
+    if (h<21){
+        slots.push({hour: h, minute:30,period:timeCheck(h)})
+    }
+}
+/*
+print all the available barber slots
+always comment it to test (!important)
+*/ 
+
+// slots.forEach(slot => {
+//     console.log(
+//         `${slot.hour.toString().padStart(2, '0')}:${slot.minute === 0 ? '00' : '30'} - ${slot.period}`
+//     );
+// });
+
+const clients=[
+{
     name:'Jonas', 
     hair:true, 
     beard:false,
-},{
+    time: timeCheck(19)
+    },{
     name:'matheus',
     hair:true,
     beard:true,
 }
 ]
 
-const timeCheck=(hour)=>{
-    if(hour>=8 && hour <12){'morning'}
-        else if (hour>=12 && hour<18){'evening'}
-            else if (hour >= 18 && hour<=21){'night'}
-    else{'closed'}
-}
+console.log(clients[0])
 
-let slots = []
-for (let h= 8; h)
-
-clientNumberMorning=2
-clientNumberEvening=4
-clientNumberNight=3
-
+// ALWAYS REMEMBER TO NODE IT WHERE THE SCRIPT IS
